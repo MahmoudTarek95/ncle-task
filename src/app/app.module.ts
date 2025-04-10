d2fc301bbb35f8a86c18673629875f20f1ca35b2
Mahmoud Tarek <mahmoudtarek59@gmail.com>
2025-04-10 03:24:39 +0200

    create the navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeLayoutComponent } from './components/home-layout/home-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeLayoutComponent
   ],
   imports: [
     BrowserModule,
